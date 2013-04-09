65d26062bdb74b1d4596ffe50b2a9f59b9145cc0
siciliana <siciliana@gmail.com>
2013-04-08 18:02:02 -0700

    new app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '8a6d0fe9e7ed7fcc3c5489370e149c2b270cdc7d85510b5a61efc1d2e89c2447a400583e22037c8d2a74ab4a8f2380728a4ac14a3c10f70a5bb4d083c67dcd21'
+DemoApp::Application.config.secret_token = '744da7676f994920a2d270ad3a5888d30469aa21a1e7a67b393910dfab3e9361d3868259c8e119a1ed27c95120b04f62e68a858c555a5c8a438f1d687a633227'
